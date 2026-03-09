5f4f2b48e9dd670f4b7165f6640e8297f6af8169
Quinn McFarland <lilmcfarlands@gmail.com>
2026-03-08 17:37:15 -0700

    Rewrote global functions into header and created files for blackjack game
    Rewrote all global functions into the utilities files and created the files for the blackjack game.

---
+++ Development Files/main.cpp
@@ -0,0 +1,52 @@
+//Library Inclusions
+#include <cstdlib> // Core library.
+#include <iostream> // Library to manage I/O streams
+#include <iomanip> // Library to manage visuals for I/O data
+#include <ctime> // Library to manage seeding of PRNG
+#include <string> // Library for strings
+
+// Header Inclusions
+#include "utilities.h"
+
+// Main Function
+int main(int argc, char** argv)
+{
+    // Set up program variables
+    std::string userInput;
+    int menu = 0;// Main menu variable for switch case
+    srand((unsigned int)time(NULL)); // Seeds the RNG
+    
+    // Main Menu
+    do
+    {
+        // Prompt user for choices
+        std::cout << "1. Blackjack" << std::endl;
+        std::cout << "2. Five Card Poker" << std::endl;
+        std::cout << "3. Exit" << std::endl;
+        std::cout << "Make a selection: ";
+        std::cin >> menu;
+        
+        // Menu switch case
+        switch(menu)
+        {
+            case 1:
+            {
+                break;
+            }
+            case 2:
+            {
+                break;
+            }
+            case 3:
+            {
+                break;
+            }
+            default:
+            {
+                std::cout << "Input not valid. Please try again." << std::endl;
+            }
+        }
+    } while(menu != 3); // Program will continuously run until user wishes to exit
+    
+    return 0;
+}
