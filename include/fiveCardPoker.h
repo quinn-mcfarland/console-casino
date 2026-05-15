67c0c076f5278a058f89b8912597ae9877d4e752
Quinn McFarland <lilmcfarlands@gmail.com>
2026-05-15 11:54:48 -0700

    Imported Five Card Poker legacy code and began refactoring and modifying it per new standards.

--- include/fiveCardPoker.h
+++ include/fiveCardPoker.h
@@ -1,10 +1,13 @@
 #ifndef FIVECARDPOKER_H
 #define FIVECARDPOKER_H
 
-// Constant Declarations
-
 // Variable Declarations
+extern std::array<int, 5> playerHand;
 
 // Function Prototypes
+void fiveCardPokerNewGame(std::array<int, 5>& handArray);
+void outputPokerHand(const std::array<int, 5>& handArray);
+void pokerExchange(std::array<int,5>& handArray);
+void pokerWinCheck(const std::array<int, 5>& handArray);
 
 #endif // !FIVECARDPOKER_H
